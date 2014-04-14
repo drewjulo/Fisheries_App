f5ff80a2514ac2f8999364475833c86b5d66e9a9
drewjulo <drewjulo@hotmail.com>
2014-04-14 16:09:49 +0300

    intial commit

---
+++ FisheriesApp/tools.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace MyFish
+{
+    public partial class Tools : PhoneApplicationPage
+    {
+        public Tools()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Panorama_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
