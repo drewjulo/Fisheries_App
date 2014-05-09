c3376553b881ff4aca3753d8ffdd6032bd0e3bf6
drewjulo <drewjulo@hotmail.com>
2014-05-09 18:13:00 +0300

    second commit

---
+++ FisheriesApp/market.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace MyFish
+{
+    public partial class Market : PhoneApplicationPage
+    {
+        public Market()
+        {
+            InitializeComponent();
+        }
+    }
+}
